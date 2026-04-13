d3b7930514a6e9b380a49e8d9182071746927ff7
mehmetcevikdev <mehmet_cvk20@outlook.com>
2026-04-13 17:28:41 +0300

    useSearchParams and useRouter states are used, and the selected values used in the search page

--- travel-and-cms/travel/app/(routes)/(home)/_components/Hero/MainSearch.tsx
+++ travel-and-cms/travel/app/(routes)/(home)/_components/Hero/MainSearch.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 import {
   Select,
@@ -7,13 +10,33 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Button } from "@/components/ui/button";
 
 const MainSearch = () => {
+  const router = useRouter();
+
+  const [formValues, setFormValues] = useState({
+    destination: "paris", //Default destination
+    activity: "hiking", //Default activity
+    duration: "0-8", //Default duration
+    price: "250-900", //Default price
+  });
+
+  const handleChange = (key, value) => {
+    setFormValues((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const handleSearch = () => {
+    const query = new URLSearchParams(formValues).toString();
+    router.push(`/search?${query}`);
+  };
+
   return (
     <div className="absolute z-50 left-1/2 transform -translate-x-1/2 top-167.5 md:top-212.5 lg:top-100 xl:top-117.5 2xl:top-142.5 container px-8">
       <div className="bg-white shadow-lg py-10 px-2 lg:px-24 rounded-md lg:rounded-full flex flex-col mt-12 lg:mt-24 lg:flex-row items-center justify-between p-4 gap-3">
-        <Select>
+        <Select
+          value={formValues.destination}
+          onValueChange={(value) => handleChange("destination", value)}
+        >
           <SelectTrigger className="w-full py-6">
             <SelectValue placeholder="Destination" />
           </SelectTrigger>
@@ -23,7 +46,10 @@ const MainSearch = () => {
             <SelectItem value="tokyo">Tokyo</SelectItem>
           </SelectContent>
         </Select>
-        <Select>
+        <Select
+          value={formValues.activity}
+          onValueChange={(value) => handleChange("activity", value)}
+        >
           <SelectTrigger className="w-full py-6">
             <SelectValue placeholder="Activity" />
           </SelectTrigger>
@@ -33,7 +59,10 @@ const MainSearch = () => {
             <SelectItem value="sightseeing">Sightseeing</SelectItem>
           </SelectContent>
         </Select>
-        <Select>
+        <Select
+          value={formValues.duration}
+          onValueChange={(value) => handleChange("duration", value)}
+        >
           <SelectTrigger className="w-full py-6">
             <SelectValue placeholder="0 Days - 8 Days" />
           </SelectTrigger>
@@ -43,7 +72,10 @@ const MainSearch = () => {
             <SelectItem value="15+">15+ Days</SelectItem>
           </SelectContent>
         </Select>
-        <Select>
+        <Select
+          value={formValues.price}
+          onValueChange={(value) => handleChange("price", value)}
+        >
           <SelectTrigger className="w-full py-6">
             <SelectValue placeholder="$250 - $900" />
           </SelectTrigger>
@@ -54,7 +86,10 @@ const MainSearch = () => {
           </SelectContent>
         </Select>
         <div className="w-full">
-          <Button className=" bg-orange-500 text-white hover:bg-orange-600 py-6 w-full ">
+          <Button
+            onClick={handleSearch}
+            className=" bg-orange-500 text-white hover:bg-orange-600 py-6 w-full "
+          >
             Find Now
           </Button>
         </div>
